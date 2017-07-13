0efcd4c773932ebf49c13fd92edf44db4ffc11f1
jethroau8888 <jethroau1996@gmail.com>
2017-07-13 15:57:36 +0800

    Create SafeMath.sol

---
+++ SafeMath.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.10;
+
+/// @title Overflow detection for uint math functions
+/// @notice Inspired from Melonport https://github.com/melonproject/melon/tree/master/contracts
+contract SafeMath is Assertive{
+contract SafeMath {
+
+    function safeAdd(uint256 x, uint256 y) internal returns(uint256) {
+      uint256 z = x + y;
+      assert((z >= x) && (z >= y));
+      return z;
+    }
+
+    function safeSubtract(uint256 x, uint256 y) internal returns(uint256) {
+      assert(x >= y);
+      uint256 z = x - y;
+      return z;
+    }
+
+    function safeMult(uint256 x, uint256 y) internal returns(uint256) {
+      uint256 z = x * y;
+      assert((x == 0)||(z/x == y));
+      return z;
+    }
+
+}
